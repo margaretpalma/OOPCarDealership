e9d73af82b339a5ea1ce3a7189e6ec693f03b6e5
margaretpalma <mpalma1496@gmail.com>
2025-10-23 16:02:08 -0400

    vehicle class

---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class Vehicle {
+
+        private int vehcileVin;
+        private int vehicleYear;
+        private String vehicleMake;
+        private String vehicleModel;
+        private String vehicleType;
+        private String vehicleColor;
+        private int
+
+
+
+
+
+
+
+}
