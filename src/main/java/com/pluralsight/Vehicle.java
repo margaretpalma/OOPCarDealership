428b55f50de825c07f44c69dc8cf131b11763433
margaretpalma <mpalma1496@gmail.com>
2025-10-24 08:46:54 -0400

    vehicle class

--- src/main/java/com/pluralsight/Vehicle.java
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -2,18 +2,25 @@ package com.pluralsight;
 
 public class Vehicle {
 
-        private int vehcileVin;
+        private int vehicleVin;
         private int vehicleYear;
         private String vehicleMake;
         private String vehicleModel;
         private String vehicleType;
         private String vehicleColor;
-        private int
+        private int odometer;
+        private double price;
 
-
-
-
-
+        public Vehicle(int vehicleVin, int vehicleYear, String vehicleMake, String vehicleModel, String vehicleType, String vehicleColor, int odometer, double price) {
+                this.vehicleVin = vehicleVin;
+                this.vehicleYear = vehicleYear;
+                this.vehicleMake = vehicleMake;
+                this.vehicleModel = vehicleModel;
+                this.vehicleType = vehicleType;
+                this.vehicleColor = vehicleColor;
+                this.odometer = odometer;
+                this.price = price;
+        }
 
 
 }
