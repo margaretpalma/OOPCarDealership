12a0a8d82985712115234dc79e453bdf45cf3a0d
margaretpalma <mpalma1496@gmail.com>
2025-10-27 11:21:51 -0400

    dealershipFileManager updates

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,11 +1,40 @@
 package com.pluralsight;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.StringReader;
+import java.util.ArrayList;
+
 public class DealershipFileManager {
 
-    //file handling
+
+        private String filename; //file read/write
+
+        public DealershipFileManager(String filename) {
+        this.filename = filename;
+    }
+
+        public Dealership getDealership(){
+            Dealership dealership = null;
+
+            try(BufferedReader br = new BufferedReader(new FileReader("DealershipFile")));
+
+            String line = br.readLine();
+
+            if (line != null) {
+
+                String[] parts = line.split("\\|");
+                String name = parts[0];
+                String address = parts[1];
+                String phone = parts[2];
+
+                dealership =
+            }
 
 
 
+
+        }
 
 
 }
