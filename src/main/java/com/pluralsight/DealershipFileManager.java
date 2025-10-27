c662a3620a7d9397b4725b0bab6d4c4da70242d0
margaretpalma <mpalma1496@gmail.com>
2025-10-27 12:57:54 -0400

    dealershipFileManager updates

--- src/main/java/com/pluralsight/DealershipFileManager.java
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,8 +1,6 @@
 package com.pluralsight;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.StringReader;
+import java.io.*;
 import java.util.ArrayList;
 
 public class DealershipFileManager {
@@ -14,27 +12,80 @@ public class DealershipFileManager {
         this.filename = filename;
     }
 
-        public Dealership getDealership(){
+        public Dealership getDealership() {
             Dealership dealership = null;
 
-            try(BufferedReader br = new BufferedReader(new FileReader("DealershipFile")));
+            try (BufferedReader br = new BufferedReader(new FileReader("DealershipFile"))) {
 
-            String line = br.readLine();
 
-            if (line != null) {
+                //dealership info name|address|phone
+                String line = br.readLine();
 
-                String[] parts = line.split("\\|");
-                String name = parts[0];
-                String address = parts[1];
-                String phone = parts[2];
+                if (line != null) {
 
-                dealership =
+                    String[] parts = line.split("\\|");
+                    String name = parts[0];
+                    String address = parts[1];
+                    String phone = parts[2];
+
+                    dealership = new Dealership(name, address, phone);
+                }
+                //------------------------
+                //vehicle lines
+
+                while ((line = br.readLine()) != null) {
+                    String[] parts = line.split("\\|");
+
+                    int vin = Integer.parseInt(parts[0]);
+                    int vehicleYear = Integer.parseInt(parts[1]);
+                    String vehicleMake = parts[2];
+                    String vehicleModel = parts[3];
+                    String vehicleType = parts[4];
+                    String vehcieleColor = parts[5];
+                    int odometer = Integer.parseInt[6];
+                    double price = Integer.parseInt[7];
+
+                    Vehicle vehicle = new Vehicle(vin, vehicleYear, vehicleMake, vehicleModel, vehicleType, vehcieleColor, odometer, price);
+
+                }
             }
+          //catching exception
+        } catch (IOException e) {
+        System.out.println("Error Reading File: " + e.getMessage());
+        }
+        return dealership;
+}
 
+        //dealership goes to file
 
+        public void saveDealership(Dealership dealership) {
+//            try (BufferedReader bw = new BufferedWriter(new FileWriter("DealershipFile"))) {
+//                bw.write(dealership.getName() + "|" + dealership.getAddress() + "|" + dealership.getPhoneNumber());
+//                bw.newLine();
+//
+//                //write vehicle
+//
+//                for (Vehicle v : dealership.getAllVehicles()) {
+//                    String vehicleLine = v.getVehicleVin() + "|"
+//                    v.getVehicleYear() + "|"
+//                    v.getVehicleMake() + "|"
+//                    v.getVehicleModel() + "|"
+//                    v.getVehicleType() + "|"
+//                    v.getVehicleColor() + "|"
+//                    v.getVehicleOdometer() + "|"
+//                    v.getVehiclePrice() + "|"
+//                    bw.write(vehicleLine);
+//                    bw.newLine();
+//                }
+            } catch (IOException e){
+        System.out.
 
-
+println("Errpr Writing File: "+e.getMessage());
         }
 
+            }
+        }
 
 }
+
+
